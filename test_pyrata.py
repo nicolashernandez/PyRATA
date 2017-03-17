8a180321ebdb09a2969e21ed9d7afa5f4b963287
Nicolas Hernandez <nicolas.hernandez@gmail.com>
2017-03-17 09:18:57 +0100

    add three quantifier any test ; one fails

--- test_pyrata.py
+++ test_pyrata.py
@@ -221,6 +221,22 @@ class TestPyrata(object):
     self.test(description, pattern, data, expected, loglevel)     
 
 
+  def test_search_quantifier_any_at_the_pattern_end_on_atomic_constraint(self, loglevel):
+    description = 'search_quantifier_any_at_the_pattern_end_on_atomic_constraint'
+    pattern = 'pos="DT" *pos="JJ"'
+    data = [{'raw':'The', 'lem':'the', 'pos':'DT'}, {'raw':'big', 'lem':'big', 'pos':'JJ'}, {'raw':'fat', 'lem':'fat', 'pos':'JJ'}, {'raw':'giant', 'lem':'giant', 'pos':'JJ'}, {'raw':'cars', 'lem':'car', 'pos':'NN'},  {'raw':'amazing', 'lem':'amaze', 'pos':'JJ'}]     
+    expected = [{'raw':'The', 'lem':'the', 'pos':'DT'}, {'raw':'big', 'lem':'big', 'pos':'JJ'}, {'raw':'fat', 'lem':'fat', 'pos':'JJ'}, {'raw':'giant', 'lem':'giant', 'pos':'JJ'}]
+    self.test(description, pattern, data, expected, loglevel)      
+
+  def test_search_quantifier_any_inside_the_pattern_on_non_present_atomic_constraint(self, loglevel):
+    # echo '0' | perl -ne '$s = "abde"; if ($s =~ /(bc?d)/) {print "$1\n"}'
+    description = 'search_quantifier_any_inside_the_pattern_on_non_present_atomic_constraint'
+    pattern = 'pos="DT" *pos="JJ" pos="NN"'
+    data = [{'raw':'The', 'lem':'the', 'pos':'DT'}, {'raw':'cars', 'lem':'car', 'pos':'NN'},  {'raw':'amazing', 'lem':'amaze', 'pos':'JJ'}]     
+    expected = [{'raw':'The', 'lem':'the', 'pos':'DT'}, {'raw':'cars', 'lem':'car', 'pos':'NN'}]
+    self.test(description, pattern, data, expected, loglevel)  
+
+
 # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 # Declare here all the tests you want to run
 # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -254,7 +270,8 @@ class TestPyrata(object):
     self.test_search_multiple_consecutive_quantifier_option_inside_the_pattern_on_atomic_constraint(myloglevel)
     self.test_search_quantifier_any_inside_the_pattern_on_atomic_constraint(myloglevel)
     self.test_search_quantifier_any_at_the_pattern_beginning_on_atomic_constraint(myloglevel)
-
+    self.test_search_quantifier_any_at_the_pattern_end_on_atomic_constraint(myloglevel)
+    #self.test_search_quantifier_any_inside_the_pattern_on_non_present_atomic_constraint(myloglevel)
 
     
 # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
