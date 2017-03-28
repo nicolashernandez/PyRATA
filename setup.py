a7f4ab4f55c0fe0873de95d908292863749c3498
nicolashernandez <nicolas.hernandez@gmail.com>
2017-03-28 11:34:53 +0200

    works on Configuring your Project

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+# https://packaging.python.org/distributing/
+setup(name='PyRATA',
+  version='1.0dev',
+  description='Python Rule-based feAture sTructure Analysis',
+  author='Nicolas Hernandez',
+  author_email='nicolas.hernandez@gmail.com',
+  url='https://github.com/nicolashernandez/PyRATA',
+  license='MIT',
+  keywords='NLP rule-based text mining',
+  install_requires=['ply'],
+)
